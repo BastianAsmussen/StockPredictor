2b8d83b065c828741075b23517dcca64ba6c19ec
Bastian Asmussen <bastianasmussen@gmail.com>
2023-06-27 10:39:23 +0200

    Added comment to `calculate_increase(...)` function.

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -2,6 +2,13 @@ pub mod predictor;
 pub mod trainer;
 
 /// Calculates the percentage increase between two numbers.
+///
+/// # Arguments
+/// * `start` - The start number.
+/// * `end` - The end number.
+///
+/// # Returns
+/// The percentage increase between the two numbers.
 pub fn calculate_increase(start: f64, end: f64) -> f64 {
     (end - start) / start * 100.0
 }
