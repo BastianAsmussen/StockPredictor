9fdb20138fe6f9607390c5d74abe2ae8b441eae3
Bastian Asmussen <bastianasmussen@gmail.com>
2023-06-27 14:18:20 +0200

    Made `calculate_increase(...)` not return a percentage.

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,14 +1,14 @@
 pub mod predictor;
 pub mod trainer;
 
-/// Calculates the percentage increase between two numbers.
+/// Calculates the increase between two numbers.
 ///
 /// # Arguments
 /// * `start` - The start number.
 /// * `end` - The end number.
 ///
 /// # Returns
-/// The percentage increase between the two numbers.
+/// The increase between the two numbers.
 pub fn calculate_increase(start: f64, end: f64) -> f64 {
-    (end - start) / start * 100.0
+    (end - start) / start
 }
