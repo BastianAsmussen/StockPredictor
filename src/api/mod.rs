fcea84577999ecfb513dc35e8d0bcd1e32536aa0
Bastian Asmussen <bastianasmussen@gmail.com>
2023-06-27 09:04:51 +0200

    Made API work with utility module.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,8 @@
-use crate::data::fetcher::TimeUnit;
+use log::error;
 
+use crate::model::calculate_increase;
 use serde::{Deserialize, Serialize};
+use crate::util::time::TimeUnit;
 
 pub mod routes;
 
@@ -8,11 +10,14 @@ pub mod routes;
 ///
 /// # Fields
 /// * `symbol` - The symbol of the stock to predict.
-/// * `time` - The time unit to use.
+/// * `time` - The time unit to use for the prediction.
+/// * `dataset_size` - The size of the dataset to use for the prediction.
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Request {
     symbol: String,
     time: TimeUnit,
+    dataset_size: TimeUnit,
 }
 
 /// The serialized response data.
@@ -23,8 +28,10 @@ pub struct Request {
 /// * `increase` - The increase.
 /// * `should_buy` - Whether or not the stock should be bought.
 #[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Response {
     request: Request,
+    error_message: Option<String>,
     predictions: Option<Vec<f64>>,
     increase: Option<f64>,
     should_buy: Option<bool>,
@@ -32,10 +39,17 @@ pub struct Response {
 
 pub async fn handle_request(info: &Request) -> Response {
     // Fetch the data.
-    let pure_data = crate::data::fetcher::fetch(&info.symbol, &info.time).await;
+    let pure_data = crate::data::fetcher::fetch(&info.symbol, &info.dataset_size).await;
     if pure_data.is_err() {
+        let error_message = format!("Failed to fetch data for symbol {}! {}",
+            info.symbol,
+            pure_data.err().unwrap()
+        );
+        error!("{}", error_message);
+
         return Response {
             request: info.clone(),
+            error_message: Some(error_message),
             predictions: None,
             increase: None,
             should_buy: None,
@@ -46,8 +60,15 @@ pub async fn handle_request(info: &Request) -> Response {
     // Convert the data.
     let mapped_data = crate::data::mapper::convert_data(&pure_data);
     if mapped_data.is_err() {
+        let error_message = format!("Failed to convert data for symbol {}! {}",
+            info.symbol,
+            mapped_data.err().unwrap()
+        );
+        error!("{}", error_message);
+
         return Response {
             request: info.clone(),
+            error_message: Some(error_message),
             predictions: None,
             increase: None,
             should_buy: None,
@@ -58,8 +79,15 @@ pub async fn handle_request(info: &Request) -> Response {
     // Train the model.
     let model = crate::model::trainer::train(&mapped_data);
     if model.is_err() {
+        let error_message = format!("Failed to train model for symbol {}! {}",
+            info.symbol,
+            model.err().unwrap()
+        );
+        error!("{}", error_message);
+
         return Response {
             request: info.clone(),
+            error_message: Some(error_message),
             predictions: None,
             increase: None,
             should_buy: None,
@@ -68,38 +96,39 @@ pub async fn handle_request(info: &Request) -> Response {
     let model = model.unwrap();
 
     // Predict the data.
-    let last_quote = pure_data.last().unwrap().1;
+    let (.., close) = pure_data.last().unwrap();
 
-    let prediction = crate::model::predictor::predict(&model, last_quote, &info.time);
-    if prediction.is_err() {
+    let predictions = crate::model::predictor::predict(&model, *close, &info.time);
+    if predictions.is_err() {
+        let error_message = format!("Failed to predict data for symbol {}! {}",
+            info.symbol,
+            predictions.err().unwrap()
+        );
+        error!("{}", error_message);
+
         return Response {
             request: info.clone(),
+            error_message: Some(error_message),
             predictions: None,
             increase: None,
             should_buy: None,
         };
     }
-    let prediction = prediction.unwrap();
+    let predictions = predictions.unwrap();
 
     // Calculate the increase.
-    let increase = crate::model::calculate_increase(last_quote, *prediction.last().unwrap());
+    let start = *close;
+    let end = *predictions.last().unwrap();
+    let increase = calculate_increase(start, end);
 
     // Calculate whether or not the stock should be bought.
-    let should_buy = crate::model::should_buy(&model, last_quote, &info.time);
-    if should_buy.is_err() {
-        return Response {
-            request: info.clone(),
-            predictions: None,
-            increase: None,
-            should_buy: None,
-        };
-    }
-    let should_buy = should_buy.unwrap();
+    let should_buy = increase > 0.0;
 
     // Return the response.
     Response {
         request: info.clone(),
-        predictions: Some(prediction),
+        error_message: None,
+        predictions: Some(predictions),
         increase: Some(increase),
         should_buy: Some(should_buy),
     }
