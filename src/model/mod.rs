ffb2c94a632b22dab2a0d505fe765c2fbaf0a3dc
Bastian <bastianasmussen@gmail.com>
2023-07-06 13:29:18 +0200

    Updated neural network.

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,44 +1,15 @@
-use crate::util::time::as_python_datetime;
 use pyo3::prelude::PyModule;
 use pyo3::Python;
-use time::OffsetDateTime;
-
-/// Calculates the increase between two numbers.
-///
-/// # Arguments
-/// * `start` - The start number.
-/// * `end` - The end number.
-///
-/// # Returns
-/// The increase between the two numbers.
-pub fn calculate_increase(start: f64, end: f64) -> f64 {
-    (end - start) / start
-}
 
 /// Predicts the stock price for a given symbol and period.
 ///
 /// # Arguments
 /// * `symbol` - The symbol of the stock.
-/// * `period` - The period to predict.
-/// * `start` - The start date.
-/// * `end` - The end date.
-/// * `testing_days` - The number of days to use for testing.
-/// * `future_days` - The number of days to predict into the future.
+/// * `days` - The number of days to predict into the future.
 ///
 /// # Returns
-/// A tuple containing the predictions, the increase and the accuracy.
-pub fn predict(
-    symbol: &str,
-    period: &str,
-    start: &OffsetDateTime,
-    end: &OffsetDateTime,
-    testing_days: u32,
-    future_days: u32,
-) -> Result<(Vec<f64>, f64, f64), Box<dyn std::error::Error>> {
-    // Map the start and end date to a Python datetime.
-    let start = as_python_datetime(start)?;
-    let end = as_python_datetime(end)?;
-
+/// A tuple containing the predictions and the RMSE of the model.
+pub fn predict(symbol: &str, days: &u32) -> Result<(Vec<f64>, f64), Box<dyn std::error::Error>> {
     // Call the Python script using Pyo3.
     Python::with_gil(|py| {
         let module_name = "predictor";
@@ -48,8 +19,8 @@ pub fn predict(
         let module = PyModule::from_code(py, &code, &file_name, module_name)?;
         let predict = module.getattr("predict")?;
 
-        let args = (symbol, start, end, period, testing_days, future_days);
-        let output: (Vec<f64>, f64, f64) = predict.call(args, None)?.extract()?;
+        let args = (symbol, *days);
+        let output: (Vec<f64>, f64) = predict.call(args, None)?.extract()?;
 
         Ok(output)
     })
--- src/model/predictor.py
+++ src/model/predictor.py
@@ -1,143 +1,61 @@
-import datetime as dt
-
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import tensorflow as tf
 import yfinance as yf
-from keras.layers import Dense, Dropout, LSTM
-from keras.models import Sequential
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
-def predict(symbol, start, end, period='max', testing_days=182, future_days=1):
-    """
-    Predicts the future stock prices of a given symbol.
-    :param symbol: The ticker symbol of the stock.
-    :param start: The start date of the training data.
-    :param end: The end date of the training data.
-    :param period: The period of the training data.
-    :param testing_days: The number of days to test the model on.
-    :param future_days: The number of days to predict.
-    :return: The predictions and the accuracy of the model.
-    """
-    value = 'Close'
-    
-    data = yf.Ticker(symbol)
-    data = data.history(period=period, start=start, end=end)
-    
-    # Prepare the data.
+def predict(ticker, future_days):
+    # Download historical stock data using yfinance
+    data = yf.download(ticker)
+
+    # Preprocess the data
+    data = data[["Close"]]  # Selecting only the 'Close' price column
+    data["Close"] = data["Close"].ffill()  # Forward fill any missing values
+
+    # Normalize the data
     scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(data[value].values.reshape(-1, 1))
+    data["Close"] = scaler.fit_transform(data["Close"].values.reshape(-1, 1))
 
-    x_train = []
-    y_train = []
-    
-    for x in range(testing_days, len(scaled_data)):
-        x_train.append(scaled_data[x - testing_days:x, 0])
-        y_train.append(scaled_data[x, 0])
-    
-    x_train, y_train = np.array(x_train), np.array(y_train)
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    
-    # Build the LSTM model.
-    epochs = 25
-    units = 50
-    batch_size = 32
-    verbose = 1
-    
-    model = Sequential()
-    
-    model.add(LSTM(units=units, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(Dropout(0.2))
-    
-    model.add(LSTM(units=units, return_sequences=True))
-    model.add(Dropout(0.2))
-    
-    model.add(LSTM(units=units))
-    model.add(Dropout(0.2))
-    
-    model.add(Dense(units=1))  # Prediction of the next closing value
-    
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
-    
-    # Test the model accuracy on existing data.
-    # Load the data.
-    test_period = period
-    test_start = end
-    test_end = dt.datetime.now().strftime('%Y-%m-%d')
+    # Create input features and target variable
+    n_steps = 10  # Number of time steps to consider
+    x, y = [], []
+    for i in range(n_steps, len(data)):
+        x.append(data.iloc[i - n_steps: i, 0].values)
+        y.append(data.iloc[i, 0])
+    x, y = np.array(x), np.array(y)
 
-    test_data = yf.Ticker(symbol)
-    test_data = test_data.history(period=test_period, start=test_start, end=test_end)
-    actual_prices = test_data[value].values
-    
-    total_dataset = pd.concat((data[value], test_data[value]), axis=0)
-    
-    model_inputs = total_dataset[len(total_dataset) - len(test_data) - testing_days:].values
-    model_inputs = model_inputs.reshape(-1, 1)
-    model_inputs = scaler.transform(model_inputs)
-    
-    # Make predictions on testing data.
-    x_test = []
-    
-    for x in range(testing_days, len(model_inputs)):
-        x_test.append(model_inputs[x - testing_days:x, 0])
+    # Split the data into training and testing sets
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, shuffle=False
+    )
 
-    x_test = np.array(x_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    # Build the neural network model
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Dense(64, activation="relu", input_shape=(n_steps,)),
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(1),
+        ]
+    )
 
-    predicted_prices = model.predict(x_test)
-    predicted_prices = scaler.inverse_transform(predicted_prices)
+    # Compile the model
+    model.compile(optimizer="adam", loss="mean_squared_error")
 
-    accuracy = calculate_increase(actual_prices[-1], predicted_prices[-1])
+    # Train the model
+    model.fit(x_train, y_train, epochs=50, batch_size=32, verbose=0)
 
-    # Predict the next day.
-    real_data = [model_inputs[len(model_inputs) + 1 - testing_days:len(model_inputs + 1), 0]]
-    real_data = np.array(real_data)
-    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-    
-    prediction = model.predict(real_data)
-    prediction = scaler.inverse_transform(prediction)
-
-    # Plot the testing predictions.
-    plt.plot(actual_prices, color='black', label=f'Actual {symbol} Price')
-    plt.plot(predicted_prices, color='green', label=f'Predicted {symbol} Price')
-    plt.title(f'{symbol} Share Price')
-    plt.xlabel('Time')
-    plt.ylabel(f'{symbol} Share Price')
-    plt.legend()
-    plt.show()
-
+    # Predict on the next n days
     predictions = []
-    for x in range(future_days):
-        predictions.append(prediction[0][0])
+    # Use the last n days from today (normalized) as input features
+    last_n_days = data["Close"].values[-n_steps:].reshape(1, -1)
+    for i in range(future_days):
+        pred = model.predict(last_n_days)
+        scaled_pred = scaler.inverse_transform(pred)
+        predictions.append(scaled_pred[0][0])
+        last_n_days = np.append(last_n_days[:, 1:], pred[0]).reshape(1, -1)
 
-        real_data = [np.append(real_data[0][1:], prediction[0])]
-        real_data = np.array(real_data)
-        real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+    # Evaluate the model on the test set
+    loss = model.evaluate(x_test, y_test)
 
-        prediction = model.predict(real_data)
-        prediction = scaler.inverse_transform(prediction)
-
-    increase = calculate_increase(actual_prices[-1], predictions[-1])
-
-    # Return the predictions, increase, and accuracy.
-    return predictions, increase, accuracy
-
-
-def calculate_increase(start_price, end_price):
-    """
-    Calculates the increase/decrease in price from the start price to the end price.
-    :param start_price: The starting price.
-    :param end_price: The ending price.
-    :return: The increase/decrease in price as a percentage.
-    """
-    return (end_price - start_price) / start_price * 100
-
-
-if __name__ == '__main__':
-    pred, inc, acc = predict('AAPL', '2012-01-01', '2021-01-01')
-
-    print(f'Predictions: {pred}')
-    print(f'Increase: {inc}%')
-    print(f'Accuracy: {acc}%')
+    return predictions, loss
